764985220f012e9ca504826b69a0b2417d7486ed
tj2132584 <117524042+tj2132584@users.noreply.github.com>
2023-12-02 09:45:58 +0800

    Create Car Pooling.java

---
+++ Car Pooling.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int n = trips.length;
+        int sum = 0;
+        int[] temp = new int[1001];
+        for(int i = 0; i < n; i++) {
+            temp[trips[i][1]] += trips[i][0];
+            temp[trips[i][2]] += (-1) * trips[i][0];
+        }
+
+        for(int t : temp) {
+            sum += t;
+
+            if(sum > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
